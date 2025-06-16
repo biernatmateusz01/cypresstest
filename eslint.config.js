eccc6eb894af409cb3e41f452ebc2ebc6c91f5ac
biernatmateusz01 <biernat521@gmai.com>
2025-06-16 20:02:11 +0200

    TASK-11: 'lint fix'

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import vue from 'eslint-plugin-vue'
+import tseslint from 'typescript-eslint'
+import prettier from 'eslint-plugin-prettier'
+
+/** @type {import("eslint").Linter.FlatConfig} */
+export default [
+  {
+    ignores: ['node_modules', 'dist', '.output', '.nuxt'],
+  },
+  ...tseslint.configs.recommended,
+  ...vue.configs['flat/recommended'],
+  {
+    files: ['**/*.ts', '**/*.vue'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'vue/multi-word-component-names': 'off',
+    },
+  },
+]
